ce0b2af4f92bccb3501c616f2e548324360f90cd
MarcoEspinozaa <marcoo.espinozaa@gmail.com>
2025-02-05 19:54:21 -0300

    correccion app.py10

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 app.secret_key = '@Admin123'
 
 # Configuración de la base de datos MySQL usando la variable de entorno MYSQL_URL
-app .config["SQLALCHEMY_DATABASE_URI"] = "mysql+pymysql://root:fhHvCmIamLdjUwmzlnROdjIiICYbiGBW@mysql.railway.internal:3306/railway"
+app .config["SQLALCHEMY_DATABASE_URI"] = "mysql+pymysql://root:fhHvCmIamLdjUwmzlnROdjIiICYbiGBW@junction.proxy.rlwy.net:23095/railway"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db.init_app(app)
